7cddffe43bc6b781bbe35a9835a0ee518d138525
Aishwarya Sejgaya <aishwaryasejgaya0240@gmail.com>
2022-06-03 21:38:04 +0530

    problem-4 and get-api assignment commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -66,25 +66,98 @@ module.exports = router;
 // //     res.send('Hello there!')
 // // });
 
-// // router.get('/candidates', function(req, res){
-// //     console.log('Query paramters for this request are '+JSON.stringify(req.query))
-// //     let gender = req.query.gender
-// //     let state = req.query.state
-// //     let district = req.query.district
-// //     console.log('State is '+state)
-// //     console.log('Gender is '+gender)
-// //     console.log('District is '+district)
-// //     let candidates = ['Akash','Suman']
-// //     res.send(candidates)
-// // })
+//-------Movies-1--------
+router.get('/movies', function (req, res) {
+  const movies=['Rang de basanti', 'The shining', 'Lord of the rings','Batman begins']
+  res.send(movies)
+});
 
-// // router.get('/candidates/:canidatesName', function(req, res){
-// //     console.log('The request objects is '+ JSON.stringify(req.params))
-// //     console.log('Candidates name is '+req.params.canidatesName)
-// //     res.send('Done')
-// // })
+router.get('/movies/:indexNumber', function (req, res) {
+  const movies=['Rang de basanti', 'The shining', 'Lord of the rings','Batman begins']
+  JSON.stringify(req.params)
+  let len = movies.length
+  let item = movies[req.params.indexNumber]
+  if(req.params.indexNumber < len){
+      res.send(item)
+  }else{
+      res.send('Error: There is no movie.')
+  }
+  
+  //console.log(req.params.indexNumber)
+});
+
+router.get('/films', function (req, res) {
+  const film = [{
+         id: 1,
+         name: "The Shining"
+        },
+        {
+         id: 2,
+         name: "Incendies"
+        },
+        {
+          id: 3,
+          name: "Rang de Basanti"
+        },
+        {
+         id: 4,
+         name: "Finding Nemo"
+        }];
+     
+     res.send(film)
+});
+
+router.get('/films/:filmId', function (req, res) {
+  const films = [{
+      id: 1,
+      name: "The Shining"
+     },
+     {
+      id: 2,
+      name: "Incendies"
+     },
+     {
+       id: 3,
+       name: "Rang de Basanti"
+     },
+     {
+      id: 4,
+      name: "Finding Nemo"
+     }
+  ]
+ // console.log(films[0])
+     JSON.stringify(req.params)
+     const len = films.length
+     const item = films[req.params.filmId]
+     if(req.params.filmId < len){
+        res.send(item)
+     }else{
+      res.send('Error: There is no movie.')
+    }
+    //res.send("adfdfa")
+});
 
 
+// router.get('/candidates', function(req, res){
+//   console.log('Query paramters for this request are '+JSON.stringify(req.query))
+//   let gender = req.query.gender
+//   let state = req.query.state
+//   let district = req.query.district
+//   console.log('State is '+state)
+//   console.log('Gender is '+gender)
+//   console.log('District is '+district)
+//   let candidates = ['Akash','Suman']
+//   res.send(candidates)
+// })
+
+// router.get('/candidates/:canidatesName', function(req, res){
+//   console.log('The request objects is '+ JSON.stringify(req.params))
+//   console.log('Candidates name is '+req.params.canidatesName)
+//   res.send('Done')
+// })
+
+
+// module.exports = router;
 
 
 
