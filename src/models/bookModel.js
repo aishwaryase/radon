eeb0deb66a4fe6e87eab180c7b03338f22bcd12e
Aishwarya Sejgaya <aishwaryasejgaya0240@gmail.com>
2022-06-06 23:13:22 +0530

    session2-part2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,15 +1,31 @@
 const mongoose = require('mongoose');
 
-
-
 const bookSchema = new mongoose.Schema( {
-
-       bookName : String,
-       autherName : String,
-       category : String,
-       year : String
-
+    bookName: String, 
+    authorName: String, 
+    tags: [String],
+    
+    isPublished: Boolean,
+    prices: {
+        indianPrice: String,
+        europePrice: String,
+    },
+    sales: {type: Number, default: 10}
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //books
+
+//Validation:
+//require:true
+//unique
+// default
+
+//String
+//Number
+//Date
+//Boolean
+// Arrays
+// Object
+// ObjectId
+// Buffer - not cover
