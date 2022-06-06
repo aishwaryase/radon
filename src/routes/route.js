1f4cc0355fd9d8cd657463e3b1bfc63829495322
Aishwarya Sejgaya <aishwaryasejgaya0240@gmail.com>
2022-06-06 22:00:44 +0530

    mongo queries, limit etc, update, async await, regexsession 2- part2

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController.js")
 const BookModel= require("../models/bookModel.js")
-const BookController= require("../controllers/bookController.js")
+const BookController = require("../controllers/bookController.js")
 
 
 router.get("/test-me", function (req, res) {
